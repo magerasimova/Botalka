1fb0dbc40fb84787edeb019a5c80b7625f44f6a3
LitYora <matveybess@gmail.com>
2021-04-10 14:55:16 +0300

    added func buttons for cancel timer

--- Botalka/ShopModule/View/ShopViewController.swift
+++ Botalka/ShopModule/View/ShopViewController.swift
@@ -11,12 +11,8 @@ class ShopViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let color = UIColor(named: "Color")
+        self.view.backgroundColor = color
     }
 
     // MARK: - Table view data source
